d8985d5e3cab230c1eb87f454624774f1f87eebe
lilys tacos <lilyannek@gmail.com>
2022-08-23 13:47:13 -0700

    bucket.js file

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -33,13 +33,9 @@ function BucketList() {
   // Function to remove bucket list item and update state
   const removeBucketItem = (id) => {
     // TODO: Write logic that will return an array of items that don't contain the ID passed to this function
-    let removeBucket = bucket.filter((item) => {
-      if (item.id === id) {
-        removeBucket.splice();
-      }
-      return item;
-    })
+    let removeBucket = bucket.filter(item => item.id !== id);
     // TODO: Update the bucket state variable
+    setBucket(removeBucket);
   };
 
   // Function to edit the bucket list item
